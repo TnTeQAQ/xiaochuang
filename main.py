ece553cbaefe32a1617693edcf148b2ea262648f
TnTeQAQ <103574636+TnTeQAQ@users.noreply.github.com>
2023-09-04 19:58:39 +0800

    'done'

--- main.py
+++ main.py
@@ -5,33 +5,38 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import GridSearchCV
 from sklearn import metrics
 from sklearn.ensemble import AdaBoostClassifier as ada
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier as RF
-from sklearn.model_selection import cross_val_score
-from sklearn.metrics import roc_auc_score
 from sklearn.ensemble import VotingClassifier
+import joblib
 
 
 # 标签编号
-def labelEncode(df):
+def labelEncode(df, s):
     dic = {}
     uni = df.unique()
     for i in range(len(uni)):
         dic[uni[i]] = i
-    print("标签转换为：", dic, '\n')
+    print("{}标签转换为：".format(s), dic, '\n')
+    with open('results/labels/{}.dic'.format(s), 'w+') as f:
+        f.write(str(dic))
+        f.close()
     return df.replace(dic), dic
-
 
 # 把不是数字的列都转为编号
 def colsEncode(df):
     for i in df.columns:
         if df[i].dtype == 'object':
-            df[i], dic = labelEncode(df[i])
+            df[i], dic = labelEncode(df[i], i)
     return df
+
+# 保存模型
+def saveModels(clf):
+    joblib.dump(clf, 'results/models/{}.pkl'.format(label))
+    print(label, '保存成功')
 
 
 # 计算准确率
@@ -41,7 +46,8 @@ def calcAR(clf):
     y_pre = clf.predict(x_test)
     print('{}在预测集模型的准确率为：'.format(label), metrics.accuracy_score(y_test, y_pre))
     print('{}在训练集模型的准确率为：'.format(label), metrics.accuracy_score(y_train, clf.predict(x_train)))
-    print('{}的综合准确率为：'.format(label), metrics.accuracy_score(y, clf.predict(X)))
+    tem = metrics.accuracy_score(y, clf.predict(x))
+    print('{}的综合准确率为：'.format(label), tem)
 
     t = pd.DataFrame(y_test - y_pre)
     print('{}标签准确率：'.format(label), len(t[t[0] == 0]) / len(t[0]))
@@ -51,12 +57,15 @@ def calcAR(clf):
     yt[yt[0] != 0] = 1
     print('{}拦截准确率：'.format(label), len(yt[((yt == t)[0] == True)]) / len(yt))
 
+    return tem
+
 
 if __name__ == '__main__':
-    train_data = pd.read_csv(r'UNSW_NB15_training-set.csv')
 
+    #################################数据预处理#################################
+    train_data = pd.read_csv(r'UNSW_NB15_training-set.csv')
     # 处理标签
-    y, ydic = labelEncode(train_data['attack_cat'])
+    y, _ = labelEncode(train_data['attack_cat'], 'attack_cat')
     y = np.array(y)
     # 处理输入数据
     X = colsEncode(train_data.drop(['attack_cat', 'label'], axis=1))
@@ -68,17 +77,39 @@ if __name__ == '__main__':
     x = tranfer.fit_transform(X)
     x_train = tranfer.transform(x_train)
     x_test = tranfer.transform(x_test)
+    #################################数据预处理#################################
 
-    # 模型实例化
-    knn = KNeighborsClassifier()
-    LR = LogisticRegression()
-    Ada = ada()
-    GBDT = GradientBoostingClassifier()
-    svc = SVC()
-    rf = RF()
 
-    # 集合模型训练
-    for clf, label in zip([knn, LR, Ada, GBDT, svc, rf],
-                          ['KNN', 'LR', 'Ada', 'GBDT', 'svc', 'rf']):
+    #################################模型训练评估#################################
+    # # 模型实例化
+    # knn = KNeighborsClassifier()
+    # LR = LogisticRegression()
+    # Ada = ada()
+    # GBDT = GradientBoostingClassifier()
+    # svc = SVC(probability=True)
+    # rf = RF()
+    #
+    # weight = []
+    #
+    # # 集合模型训练并评估
+    # for clf, label in zip([knn, LR, Ada, GBDT, svc, rf],
+    #                       ['knn', 'LR', 'Ada', 'GBDT', 'svc', 'rf']):
+    #     clf.fit(x_train, y_train)
+    #     weight.append(calcAR(clf))
+    #     saveModels(clf)
+    #
+    # label = 'vote'
+    # w = weight/sum(weight)
+    # vote = VotingClassifier(estimators=[('knn', knn), ('LR', LR), ('Ada', Ada),
+    #                                     ('GBDT', GBDT), ('svc', svc), ('rf', rf)],
+    #                         voting='soft', weights=weight)
+    # vote.fit(x_train, y_train)
+    # saveModels(vote)
+    # calcAR(vote)
+
+    # 读取模型并评估
+    for label in zip(['knn', 'LR', 'Ada', 'GBDT', 'svc', 'rf', 'vote']):
+        clf = joblib.load('results/models/{}.pkl'.format(label))
         clf.fit(x_train, y_train)
         calcAR(clf)
+    #################################模型训练评估#################################
